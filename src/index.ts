5a417cdc1c771dd5b179243c40d98bcd7a09e813
ajchen02 <88434425+ajchen02@users.noreply.github.com>
2025-07-22 17:31:43 +0800

    working

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export interface Config {
   seed: string
   enablePic: boolean
   enableAudio: boolean
-  enableVideo: boolean
 }
 
 export const Config: Schema<Config> = Schema.intersect([
@@ -18,32 +17,44 @@ export const Config: Schema<Config> = Schema.intersect([
   Schema.object({
     enablePic: Schema.boolean().default(false).description('开启图床服务'),
     enableAudio: Schema.boolean().default(true).description('开启音床服务'),
-    enableVideo: Schema.boolean().default(true).description('开启视频床服务')
   }).description("启用功能")
 ])
 
 export const inject = ['filemanager'];
 
+
 export function apply(ctx: Context, config: Config){
   // write your plugin here
-  //console.log(ctx.scope)
 
-  ctx.filemanager.img.reg('filebin', config.endpoint, async (file: Buffer, fileName: string) =>
+  async function upload(file: Buffer, fileName: string){
   {
-    const url = config.endpoint;
     const binNum = String(Math.floor(Date.now()/1000/60/60/24/5))
-
     const bin = config.seed + binNum
-    const data = await ctx.filemanager.axios.post(`${url}/${bin}/${fileName}`, file, {
+    const url = `${config.endpoint}/${bin}/${Math.random().toString(36).slice(-8)}`;
+    const data = await ctx.filemanager.axios.post(url, file, {
       headers: {
         "Content-Type": "application/octet-stream",
       }
     });
 
-    return `${url}/${bin}/${fileName}`;
-  });
+    const red = await ctx.http(url,{
+      method:"GET",
+      redirect:"manual",
+      headers:{
+        cookie: "verified=2024-05-24"
+      }
+    })
+
+    return `${red.headers.get('location')}#${fileName}`;
+  }
+}
+
+  //console.log(ctx.scope)
+  if (config.enablePic){ctx.filemanager.img.reg('filebin', config.endpoint, upload)}
+  if (config.enableAudio){ctx.filemanager.audio.reg('filebin', config.endpoint, upload)}
 
   ctx.on('dispose', () => {
-    ctx.filemanager.img.unReg('filebin')
+    if (config.enablePic){ctx.filemanager.img.unReg('filebin')}
+    if (config.enableAudio){ctx.filemanager.audio.unReg('filebin')}
   })
 }
